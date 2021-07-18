0a8a3f9dd509ec119a86352f867952c470639177
yaser shakoori <yasershakoori@outlook.com>
2021-07-18 21:32:28 +0430

    ProductCategory Imelemented

---
+++ LampShade/ShopManagement.Application.Contracts/ProductCategory/IProductCategoryApplication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopManagement.Application.Contracts.ProductCategory
+{
+    public interface IProductCategoryApplication
+    {
+        void Create(CreateProductCategory command);
+        void Edit(EditProductCategory command);
+        Domain.ProductCategoryAgg.ProductCategory Get(long id);
+        List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel);
+
+    }
+}
